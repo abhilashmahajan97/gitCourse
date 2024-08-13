90b168129372ae824b05c37d032aed47afacd83e
abhilash <abhilashpmahajan@gmail.com>
2024-08-13 19:44:05 +0530

    first commit

---
+++ src/test/java/Cucumber/TestNGRunner.java
@@ -0,0 +1,17 @@
+package Cucumber;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/java/Cucumber", glue = "BookingHotel.stepDefination", monochrome = true, plugin = {
+		"html:target/cucumber.com" })
+
+public class TestNGRunner extends AbstractTestNGCucumberTests {
+
+	/*
+	 * @Override
+	 *
+	 * @DataProvider(parallel = true) public Object[][] scenarios() { return
+	 * super.scenarios(); }
+	 */
+}
